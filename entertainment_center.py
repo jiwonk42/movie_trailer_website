2a29f84b12295743418495889db33ea9f3cdc78a
Jiwon Kang <jiwonk42@gmail.com>
2018-01-28 15:38:57 -0800

    add a movie Grinch

---
+++ entertainment_center.py
@@ -0,0 +1,12 @@
+import media
+#media: another file that has Movie Class
+
+#media: File Name
+#Movie: Class Name 
+grinch = media.Movie(
+    "How the Grinch Stole Christmas",
+    "A grumpy Grinch plots to ruin Christmas for the village of Whoville.",
+    "http://t3.gstatic.com/images?q=tbn:ANd9GcT-oosslYzD61pdLgPVmjN53hdzb-bjBeJajr2UILlq7kQNz7jR",
+    "https://www.youtube.com/watch?v=myTaigPrbsg")
+
+print(grinch.storyline)
---
+++ media.py
@@ -0,0 +1,9 @@
+class Movie():
+    #Init function creates memory space for properties of the class
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+    
