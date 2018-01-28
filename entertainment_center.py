416b3b8295203308eb1e61e09baa3ff95bd043da
Jiwon Kang <jiwonk42@gmail.com>
2018-01-28 15:42:19 -0800

    add a movie Orphan

--- entertainment_center.py
+++ entertainment_center.py
@@ -9,4 +9,10 @@ grinch = media.Movie(
     "http://t3.gstatic.com/images?q=tbn:ANd9GcT-oosslYzD61pdLgPVmjN53hdzb-bjBeJajr2UILlq7kQNz7jR",
     "https://www.youtube.com/watch?v=myTaigPrbsg")
 
-print(grinch.storyline)
+orphan = media.Movie(
+    "Orphan",
+    "A husband and wife who recently lost their baby adopt a 9 year-old girl who is not nearly as innocent as she claims to be.",
+    "http://www.gstatic.com/tv/thumb/movieposters/193289/p193289_p_v8_aa.jpg",
+    "https://www.youtube.com/watch?v=2ywOPNNii9w")
+
+print(orphan.storyline)
